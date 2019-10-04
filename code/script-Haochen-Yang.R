7c326a4b57229ff7876718d9113055f22106417a
杨昊辰 <yanghaochen@yanghaochendeMacBook-Pro.local>
2019-10-03 17:01:17 -0700

    add script

---
+++ code/script-Haochen-Yang.R
@@ -0,0 +1,11 @@
+# ===================================================================
+# Title: Descriptive Analysis
+# Description:
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Haochen Yang
+# Date: 10-3-2019
+# ===================================================================
+
